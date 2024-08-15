b9d59f4c5d800493ed095a03c3aea657456c5ccb
Omar Ghamrawi <omarghamrawi18@gmail.com>
2024-08-15 10:49:51 +0300

    Travis Config

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,20 +1,21 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('../factories/sessionFactory');
-const userFactory = require('../factories/userFactory');
+const puppeteer = require("puppeteer");
+const sessionFactory = require("../factories/sessionFactory");
+const userFactory = require("../factories/userFactory");
 
 class CustomPage {
   static async build() {
     const browser = await puppeteer.launch({
-      headless: false
+      headless: true,
+      args: ["--no-sandbox"],
     });
 
     const page = await browser.newPage();
     const customPage = new CustomPage(page);
 
     return new Proxy(customPage, {
-      get: function(target, property) {
+      get: function (target, property) {
         return customPage[property] || browser[property] || page[property];
-      }
+      },
     });
   }
 
@@ -26,25 +27,25 @@ class CustomPage {
     const user = await userFactory();
     const { session, sig } = sessionFactory(user);
 
-    await this.page.setCookie({ name: 'session', value: session });
-    await this.page.setCookie({ name: 'session.sig', value: sig });
-    await this.page.goto('localhost:3000/blogs');
+    await this.page.setCookie({ name: "session", value: session });
+    await this.page.setCookie({ name: "session.sig", value: sig });
+    await this.page.goto("http://localhost:3000/blogs");
     await this.page.waitFor('a[href="/auth/logout"]');
   }
 
   async getContentsOf(selector) {
-    return this.page.$eval(selector, el => el.innerHTML);
+    return this.page.$eval(selector, (el) => el.innerHTML);
   }
 
   get(path) {
-    return this.page.evaluate(_path => {
+    return this.page.evaluate((_path) => {
       return fetch(_path, {
-        method: 'GET',
-        credentials: 'same-origin',
+        method: "GET",
+        credentials: "same-origin",
         headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json());
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
     }, path);
   }
 
@@ -52,13 +53,13 @@ class CustomPage {
     return this.page.evaluate(
       (_path, _data) => {
         return fetch(_path, {
-          method: 'POST',
-          credentials: 'same-origin',
+          method: "POST",
+          credentials: "same-origin",
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify(_data)
-        }).then(res => res.json());
+          body: JSON.stringify(_data),
+        }).then((res) => res.json());
       },
       path,
       data
